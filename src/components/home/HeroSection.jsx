c6eb7aca1397858e9d7a65065388eb78b4d93194
dimasnugrahs <79310230+dimasnugrahs@users.noreply.github.com>
2025-12-05 16:47:24 +0800

    vercel deploy config

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -3,8 +3,16 @@ import { Link } from "react-router-dom";
 export default function HeroSection() {
   return (
     <>
-      <div>Hello World</div>
-      <Link to="/dashboard">Sign to</Link>
+      <div className="min-h-screen flex justify-center items-center">
+        <div>
+          <Link
+            to="/dashboard"
+            className="bg-company-600 hover:bg-company-700 text-white rounded px-8 py-4"
+          >
+            Sign to Dashboard
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
